c2dff77c6dbc90cd777962b10f867b128b2adb8c
daikanshan <daikanshan@163.com>
2015-07-12 10:43:33 +0800

    简单实现fib基本功能

--- B0/fib.rb
+++ B0/fib.rb
@@ -1,1 +1,22 @@
-#
+#简单实现基本功能
+def fib(num)
+        i = 1;
+        swp1 = 1
+        swp2 = 1
+        sum  = 1
+        while i <=num do
+                if (i == 1 || i == 2)
+                        sum = 1
+                else
+                        sum = swp1+swp2
+                        swp1 = swp2
+                        swp2 = sum
+                end
+                puts "fib(#{i})=#{sum}\n"
+                i+=1
+        end
+end
+puts "请输入1-100之间的数"
+num = STDIN.gets
+fib(num.to_i);
+
