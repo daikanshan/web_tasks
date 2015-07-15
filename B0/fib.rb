8929d4c6402085b4812c01e67244e5a337cda209
daikanshan <daikanshan@163.com>
2015-07-15 10:39:51 +0800

    优化算法

--- B0/fib.rb
+++ B0/fib.rb
@@ -1,31 +1,31 @@
 #简单实现基本功能
-def fib(num)
-	i = 1;
-    swp1 = 1
-    swp2 = 1
-    sum  = 1
-    while i <=num do
-        if (i == 1 || i == 2)
-                sum = 1
-        else
-                sum = swp1+swp2
-                swp1 = swp2
-                swp2 = sum
-        end
-        puts "fib(#{i})=#{sum}\n"
-        i+=1
-    end
+def fib(max_num,fib_val=[1,1])
+	i = 3;
+  swp1 = 1
+  swp2 = 1
+  sum  = 1
+  while i <= max_num do 
+    sum = swp1+swp2
+    swp1 = swp2
+    swp2 = sum
+	  fib_val.push(sum)
+    i+=1
+  end
+	fib_val
 end
+fib_val=fib(100)
 while true do
 	puts "请输入1-100之间的数"
 	num = STDIN.gets
 	begin
-		if(!(num.to_i.to_s==num.chomp))
+		if(num.to_i.to_s!=num.chomp)
 			raise "不是整数！"
 		elsif(num.to_i>100||num.to_i<=0)
 			raise "不在有效范围内"
 		else
-			fib(num.to_i)
+			(1..num.to_i).each do |i|
+				puts "fib(#{i})=#{fib_val[i-1]}"
+			end
 		end
 	rescue => e
 		puts e.message
