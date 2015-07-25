5b54f3a1fb5b2f4cea7a47a44116a4d45c279869
daikanshan <daikanshan@163.com>
2015-07-25 22:32:39 +0800

    用数据库完成用户注册/登陆/发表留言

--- B2/index.rb
+++ B2/index.rb
@@ -1,32 +1,87 @@
 require 'sinatra'
+require 'mysql2'
+require 'active_record'
 require 'erb'
+
+use Rack::Session::Pool, :expire_after => 1200 
+
+configure do
+	enable :sessions
+end
+#Connection
+ActiveRecord::Base.establish_connection(
+	adapter: 'mysql2',
+	host:    'localhost',
+	database:'web_task',
+	username:'root',
+	password:'310210'
+)
+#Model
+class User < ActiveRecord::Base
+
+end
+
+class Message < ActiveRecord::Base
+end
+#routes
 get '/' do
 	@title = "首页"
-	if params[:select]=='author'#查询用户
-		author = params[:value]
-		authorInfo = Gloinfo.new("globleInfo.txt").authorInfo#获取所有用户信息
-		if authorInfo.has_key?(author)#是否有此用户
-			@info = authorInfo[author].reverse
-		else
-			@info = []
-			@error = "没有此用户的留言！"
-		end
-	elsif params[:select]=='id'#查询id
-		id = params[:value]
-		@info = Gloinfo.new("globleInfo.txt").current#获取当前所有ID
-		if @info.include?(id)#是否有此ID
-			@info = [id]
-		else
-			@info = []
-			@error = "没有此ID的留言！"
-		end
-	else
-		@info = Gloinfo.new("globleInfo.txt").current #get current id to show in the index
-		if @info.class!=NilClass
-			@info = @info.reverse
-		end
-	end
+	@messages = Message.all.order('created_at DESC')
   erb :index
+end
+
+get '/login' do
+	erb :login
+end
+post '/login' do
+	@error = [] #收集登陆错误信息
+	name = params['username']
+	password = params['password']
+	user = User.find_by(name:name,password:password)
+	if user.nil?
+		@error<<'用户名或密码错误！'
+		redirect to'/login'
+	else
+		session['username'] = name
+		session['user_id'] = user.id
+		redirect to'/'
+	end
+end
+
+get '/register' do
+	erb :register
+end
+post '/register' do
+	@error = []#收集注册错误信息 
+	name = params['username']
+	password = params['password']
+	confirm = params['confirm']
+	if name.empty? 
+		@error<<'用户名不能为空'
+	elsif name.to_i.to_s==name
+		@error<<'用户名不能为数字'
+	end
+	if password.empty?
+		@error<<'密码不能为空'
+	elsif password.length<6 
+		@error<<'密码不能小于6位'
+	end
+	if not password == confirm 
+		@error<<'密码确认失败'
+	else
+	end
+	if @error==[] #提交的form表单没有错误
+		@success = true
+	else
+		@success = false
+	end
+	if @success #注册信息齐全
+		user = User.create(name:name,password:password)
+		session['username'] = name
+		session['user_id']=user.id
+		redirect to '/'
+	end
+	erb :hint
 end
 
 get '/edit' do
@@ -38,158 +93,35 @@ get '/add' do
 end
 
 post '/add' do
+	if session['username'].nil?
+		redirect to"/login"
+	end
+	@error = []
 	@title = "添加留言"
-	@error = ''#to make sure having all needed params
-	if params[:author].nil? or params[:author]==''#用户名为空
-		@error= "author should not be null"
+	content = params['message']
+	if content.empty?
+		@error<<'留言不得为空！'
+	elsif content.length<10
+		@error<<'留言不得小于10个字'
 	end
-	if params[:author].to_i!=0#数字开头不合法
-		@error= "名称不合法！"
-	end
-	if params[:message].nil? or params[:message]==''#留言为空
-		@error= "message should not be null"
-	end
-	if params[:message].length<10#留言少于10个子
-		@error= "留言不得小于十个字！"
-	end
-	if @error==''
-		@error="添加成功！"
-		author = params[:author]
-		message = params[:message]
-		result = CURD.add(author,message)#添加留言信息
+	if @error == []
+		message = Message.create(user_id:session['user_id'],
+														content:content,
+														created_at:Time.now.to_i.to_s,
+														modified_at:Time.now.to_i.to_s)
+		redirect to '/'
 	end
 	erb :add
 end
 
 post '/delete/:id' do
 	@title = "删除留言"
-	id = params[:id]#因为是POST不用判断id
-	CURD.delete(id)	#删除留言信息
 	erb :delete
 end
 
-class CURD 
-	def self.add(author,message)
-		info = Gloinfo.new("globleInfo.txt")#get current global information
-		id = info.id.to_i + 1#id add 1
-		if info.isNil
-			current = [id]
-			authorInfo={}
-			authorInfo[author]=[id]
-		else
-			current = info.current<<[id]#get current showing messages's id to add a new one
-			authorInfo = info.authorInfo
-			if authorInfo.has_key?(author) 
-				authorInfo[author]<<[id]
-			else
-				authorInfo[author]=[id]
-			end
-		end
-		file = File.open(File.join("messages","#{id}.txt"),"w+")#新建一个留言文件
-		file.puts "id:#{id}"
-		file.puts "author:#{author}"
-		file.puts "created_at:"+Time.now.to_s
-		file.puts "<message>"
-		file.puts message
-		file.close
-
-		file = File.open("globleInfo.txt","w+")#打开全局信息文件并作修改
-		file.puts "id:#{id}"
-		file.puts current.join(",")
-		authorInfo.each do |key,value|
-			file.puts"<author>"
-			file.puts key
-			file.puts value.join(",")
-		end	
-		file.close
-		return id
-	end
-
-	def self.delete(id)
-		ids = []
-		if id.class==Array
-			ids = id
-		else
-			ids << id
-		end
-		info = Gloinfo.new("globleInfo.txt")
-		currentId = info.id
-	  current = info.current-ids#删除全局文件中记录的id
-		authorInfo = {}
-		ids.each do |id|#修改全局信息并且删除相关message文件
-			author = Message.new(File.join("messages","#{id}.txt")).author
-			File.delete(File.join("messages","#{id}.txt"))
-			authorInfo = info.authorInfo
-			authorInfo[author]=authorInfo[author]-[id]
-			if authorInfo[author]==[] 
-				authorInfo.delete(author)
-			end
-		end
-		file = File.open("globleInfo.txt","w+")#写入全局文件信息
-		file.puts "id:#{currentId}"
-		file.puts current.join(",")
-		authorInfo.each do |key,value|
-			file.puts"<author>"
-			file.puts key
-			file.puts value.join(",")
-		end
-		file.close
-	end
-
-	def self.queryID(id)#查询id
-		info = Gloinfo.new("globleInfo.txt")
-		if info.current.include?(id)
-			return id
-		else
-			return 404
-		end
-	end
-
-	def self.queryAuthor(author)#查询作者
-		info = Gloinfo.new("globleInfo.txt")
-		if info.authorInfo.has_key?(author)
-			return info.authorInfo[author]
-		else
-			return 404
-		end
-	end
-end
-
-class Message
-	attr_reader :id , :author , :created_at , :message
-	def initialize(file)
-		content = File.readlines(file)#获取留言信息
-		@id = content[0].split(":")[1].chomp#获取留言ID
-		@author = content[1].split(":")[1].chomp#获取留言作者
-		@created_at = content[2].split(":")[1].chomp#获取留言时间
-		@message = content.join.split("<message>")[1]#获取留言内容
-		  
-	end
-end
-
-class Gloinfo
-	attr_reader :id , :current, :authorInfo ,:isNil
-	def initialize(file)
-		content = File.readlines(file)#获取全局信息
-		@id = content[0].split(":")[1].chomp#获取当前ID
-		if content[1].class!=NilClass&&content[1]!="\n"#当有留言可显示
-			@isNil = false
-			@current = content[1].chomp.split(",")#获取当前要显示的留言id
-			content = content[2..-1].join.split("<author>")[1..-1]
-			@authorInfo = {}
-			content.each do |i|
-				@authorInfo[i.split("\n")[1]] = i.split("\n")[2].split(",")#获取所有记录作者的留言id
-			end
-		else#当前没有留言可显示
-			@isNil = true
-			con = File.open("globleInfo.txt","w+")
-			con.puts"id:0"#重置id起始位置
-			con.close
-			@current = []
-			@authorInfo = {}
-		end
-	end
-end
 not_found do
 	404
 end
+after do
+  ActiveRecord::Base.clear_active_connections!
+end  
