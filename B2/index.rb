60063b97fd1f9be9e87fb90feadb7d2d4a17c049
daikanshan <daikanshan@163.com>
2015-08-13 07:44:30 +0800

    修改部分验证逻辑

--- B2/index.rb
+++ B2/index.rb
@@ -3,64 +3,54 @@ require 'mysql2'
 require 'active_record'
 require 'erb'
 
-use Rack::Session::Pool, :expire_after => 1200 
+require './models/user.rb'
+require './models/message.rb'
+
+use Rack::Session::Pool, :expire_after => 1200
+
+#Connection
+dbconfig = YAML::load(File.open(File.dirname(__FILE__)+'/database.yaml'))
+ActiveRecord::Base.establish_connection(dbconfig)
 
 configure do
 	enable :sessions
 end
-#Connection
-ActiveRecord::Base.establish_connection(
-	adapter: 'mysql2',
-	host:    'localhost',
-	database:'web_task',
-	username:'root',
-	password:'310210'
-)
-#Model
-class User < ActiveRecord::Base
 
-end
-
-class Message < ActiveRecord::Base
-end
 #routes
 get '/' do
 	@title = "首页"
 	@messages = []
 	search_type = params['select']
 	search_value = params['value']
-	if search_type.nil? ||search_type.empty?
-		@messages = Message.all.order('created_at DESC')
+	if search_type.nil? ||search_type.empty? || search_value.nil? || search_value.empty?
+		@messages = Message.all.order(created_at: :DESC)
 	elsif search_type == 'id' #用户查询id
-		begin
-			id = Message.find(search_value)
-			if not id.nil?
-				@messages<<id
-			else
-				raise "没有该ID的留言！"
-			end
-		rescue => e
-			@error = "没有该ID的留言信息！"
-			
-		end
-	elsif search_type == 'author' #用户查询留言作者
-		begin
-			user = User.find_by(name: search_value)
-			if not user.nil?
-				user_id = user.id
-			else
-				raise "没有该用户的留言信息！"
-			end
-			@messages = Message.where("user_id =?",user_id).order('created_at DESC')
-		rescue => e
-			@error = e.message
-		end
+    msg = Message.find(search_value)
+    if not msg.nil?
+      @messages<<msg
+    else
+      @error = "没有该ID的留言！"
+    end
+  elsif search_type == 'author' #用户查询留言作者
+    User.all.each do |user|
+      if user.name.include?(search_value)
+        Message.where(user_id: user.id).order(created_at: :DESC).each do |msg|
+          @messages << msg
+        end
+      end
+    end
+
+    if not @messages.nil?
+
+    else
+      @error = "没有该用户的留言信息！"
+    end
 	end
   erb :index
 end
 get '/me' do
 	if !session['username'].nil?
-		@messages = Message.where("user_id=?",session['user_id']).order('created_at DESC')
+		@messages = Message.where(user_id: session['user_id']).order(created_at: :DESC)
 	end
 	erb :index
 end
@@ -71,7 +61,7 @@ post '/login' do
 	@error = [] #收集登陆错误信息
 	name = params['username']
 	password = params['password']
-	user = User.find_by(name:name,password:password)
+	user = User.find_by(name: name,password: password)
 	if user.nil? #登陆信息有误，重新登陆
 		@error<<'用户名或密码错误！'
 		redirect to'/login'
@@ -86,41 +76,25 @@ get '/register' do
 	erb :register
 end
 post '/register' do
-	@error = []#收集注册错误信息 
+	@error = []#收集注册错误信息
 	name = params['username']
 	password = params['password']
 	confirm = params['confirm']
-	if name.empty? 
-		@error<<'用户名不能为空'
-	elsif name.to_i.to_s==name
-		@error<<'用户名不能为数字'
-	end
-	if password.empty?
-		@error<<'密码不能为空'
-	elsif password.length<6 
-		@error<<'密码不能小于6位'
-	end
-	if not password == confirm 
-		@error<<'密码确认失败'
-	else
-	end
-	if @error==[] #提交的form表单没有错误
-		@success = true
-	else
-		@success = false
-	end
-	if @success #注册信息齐全
-		user = User.find_by(name:name)
-		if not user.nil?
-			@error<<"该用户已注册！"
-			@success = false
-		else
-			user = User.create(name:name,password:password)
-			session['username'] = name
-			session['user_id']=user.id
-			redirect to '/'
-		end
-	end
+  if password == confirm
+    user = User.new(name: name,password: password)
+    if user.valid?
+      user.save
+      session['username'] = name
+      session['user_id']=user.id
+      redirect to '/'
+    else
+      user.errors.full_messages.each do |msg|
+        @error << msg
+      end
+    end
+  else
+    @error<<'密码确认失败'
+  end
 	erb :hint
 end
 
@@ -155,10 +129,11 @@ post '/add' do
 		@error<<'留言不得小于10个字'
 	end
 	if @error == []
-		message = Message.create(user_id:session['user_id'],
+		message = Message.new(user_id:session['user_id'],
 														content:content,
 														created_at:Time.now,
 														modified_at:Time.now)
+    message.save
 		redirect to '/'
 	end
 	erb :add
