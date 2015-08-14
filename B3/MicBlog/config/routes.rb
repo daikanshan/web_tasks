275379b6dc407a272265e1d8811c58c249f452a7
daikanshan <daikanshan@163.com>
2015-08-14 11:51:44 +0800

    Finish layout and routs

--- B3/MicBlog/config/routes.rb
+++ B3/MicBlog/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
 	root 'static_pages#home'
 
   get 'help' => 'static_pages#help'
   get 'login' => 'static_pages#login'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
