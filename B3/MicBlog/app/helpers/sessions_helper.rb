42a5009c139c4562109b06f7e3818d4b17e9e180
daikanshan <daikanshan@163.com>
2015-08-14 22:31:39 +0800

    finish updating -users

--- B3/MicBlog/app/helpers/sessions_helper.rb
+++ B3/MicBlog/app/helpers/sessions_helper.rb
@@ -11,7 +11,19 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def current_user?(user)
+    user == current_user
+  end
   def logged_in?
     !current_user.nil?
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
--- B3/MicBlog/db/schema.rb
+++ B3/MicBlog/db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814044433) do
+ActiveRecord::Schema.define(version: 20150814141204) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
