269f0efceb1ca2748375d2b4c245723570c84146
daikanshan <daikanshan@163.com>
2015-08-14 11:24:06 +0800

    添加视图

--- B3/MicBlog/config/routes.rb
+++ B3/MicBlog/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 	root 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
+  get 'login' => 'static_pages#login'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
